7dee64f236e975c36b3543d61bb772407944d61d
VrushaliPatil17 <patil.vrushali93@gmail.com>
2018-07-18 17:10:00 -0700

    Add files via upload

---
+++ volcons.py
@@ -0,0 +1,77 @@
+from neo4j.v1 import GraphDatabase
+import csv
+class BookmarksExample():
+    
+    def __init__(self, uri, user, password):
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
+    def close(self):
+        self._driver.close()
+    
+    def add_person_linkedin(self, name, email, job_type, company):
+        with self._driver.session() as session:
+            emp="no"
+            if company=="persistent systems":
+                emp="yes"
+            offering = ""
+            session.run("MERGE(emp:Person {first_name: $name, email: $email, job_type: $job_type,employee:$emp})"
+                          "MERGE (com: Company {name: $company, offering: $offering})"
+                          "MERGE (emp)-[:WORKS_FOR]->(com)",
+                          name=name, email = email, job_type = job_type, company = company,emp=emp, offering = offering)
+    @staticmethod
+    def match_person_nodes(tx):
+        return list(tx.run("MATCH (a:Person) RETURN a.name AS name"))
+    
+    def add_person_crm(self,customer,employee,company,touch_point,short_term,medium_term,long_term, offering):
+        with self._driver.session() as session:
+            empY="yes"
+            empN="no"
+            compY="persistent systems"
+            email=""
+            job_type=""
+            session.run("MERGE (empYes:Person{first_name: $employee, email: $email, job_type: $job_type,employee:$empY})"
+                        "MERGE (empNo:Person{first_name: $customer,email: $email, job_type: $job_type,employee:$empN})"
+                        "MERGE (empYes)-[:CONNECTED{touch_point:$touch_point,s_term:$s_term,m_term:$m_term,l_term:$l_term}]->(empNo)"
+                        "MERGE (comYes: Company {name: $compY, offering: $offering})"
+                        "MERGE (empYes)-[:WORKS_FOR]->(compYes)"
+                        "MERGE (compNo:Company {name: $compN, offering: $offering})"
+                        "MERGE (empNo)-[:WORKS_FOR]->(compNo)",
+                        employee=employee,customer=customer,empY=empY,empN=empN,
+                        touch_point=touch_point,compY=compY,compN=company,
+                        email=email,job_type=job_type,s_term=short_term,m_term=medium_term,l_term=long_term, offering=offering)
+    
+    def add_person_meta(self, role, offer):
+        with self._driver.session() as session:
+            for record in session.run("MATCH (emp:Person)"
+                                 "WHERE emp.job_type = {role} "
+                                 "RETURN emp", role=role):
+                session.run("MERGE(off:Offer{role:$role,offering:$offer})"
+                            "MERGE(emp)-[:PROVIDES]->(off)",role = role, offer = offer)
+                
+#    def query(self,role,offer):
+#        with self._driver.session() as session:
+#            session.run("MATCH ",
+#                        role = role, offer = offer)
+    
+uri = "bolt://localhost:7687"
+book = BookmarksExample(uri, "neo4j", "pooja")
+
+with open('Linked contacts - SavedContactList.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    next(readCSV, None)
+    for row in readCSV:
+        print(row)
+        book.add_person_linkedin(row[0],row[1],row[3],row[2])
+        
+with open('CRM Data.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter=',')
+    next(readCSV, None)
+    for row in readCSV:
+        print(row)
+        book.add_person_crm(row[0],row[1],row[2],row[3],row[4],row[5],row[6], row[7])
+        
+with open('Meta data file - Role Vs offerings.csv') as csvfile:
+    readCSV = csv.reader(csvfile, delimiter='/t')
+    next(readCSV, None)
+    for row in readCSV:
+#        print(row)
+        book.add_person_meta(row[0],row[1])
